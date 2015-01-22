ddaeafd3f35667f425b32e17bed296205229f067
diegomg <dmartinezgarcia@outlook.com>
2015-01-22 23:59:15 +0100

    Add exercise 5.

--- Lesson 4 - Program Looping/main.c
+++ Lesson 4 - Program Looping/main.c
@@ -59,7 +59,7 @@
 
             5!  =  5 x 4 x 3 x 2 x 1  =  120
 
-    Write a program to generate and print a table of the first 10 factorials.
+        Write a program to generate and print a table of the first 10 factorials.
 *********************************************************/
 
 //#include <stdio.h>
@@ -76,3 +76,40 @@
 //    }
 //    return 0;
 //}
+
+/*********************************************************
+    Exercise 5
+        The following perfectly valid C program was written without much attention paid to its format.
+        As you will observe, the program is not very readable. (And believe it or not, it is even possible to make this program significantly more unreadable!)
+        Using the programs presented in this chapter as examples, reformat the program so that it is more readable. Then type the program into the computer and run it.
+
+        #include <stdio.h>
+        int main(void){
+        int n,two_to_the_n;
+        printf("TABLE OF POWERS OF TWO\n\n");
+        printf(" n     2 to the n\n");
+        printf("---    ---------------\n");
+        two_to_the_n=1;
+        for(n=0;n<=10;++n){
+        printf("%2i        %i\n",n,two_to_the_n); two_to_the_n*=2;}
+        return 0;}
+*********************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+    int n, two_to_the_n;
+
+    printf("TABLE OF POWERS OF TWO\n\n");
+    printf(" n     2 to the n\n");
+    printf("---    ---------------\n");
+    two_to_the_n = 1;
+
+    for(n = 0; n <= 10; ++n)
+    {
+        printf("%2i        %i\n", n, two_to_the_n);
+        two_to_the_n *= 2;
+    }
+
+    return 0;
+}
