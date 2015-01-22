21ff9d7c3db55c6ea2684a583209e53d77738b6c
diegomg <dmartinezgarcia@outlook.com>
2015-01-23 00:54:35 +0100

    Add Exercise 11

--- Lesson 4 - Program Looping/main.c
+++ Lesson 4 - Program Looping/main.c
@@ -216,3 +216,27 @@
 //     return 0;
 //}
 
+/*********************************************************
+    Exercise 11
+        Write a program that calculates the sum of the digits of an integer. For example, the sum of the digits of the number 2155 is 2 + 1 + 5 + 5 or 13. The program should accept any arbitrary integer typed in by the user.
+*********************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 0, c = 0;
+
+    printf("Please enter a positive integer number: ");
+    scanf("%i", &n);
+    printf("The number introduced was %i.\n", n);
+
+    while (n) {
+        c += n % 10;
+        n /= 10;
+    }
+
+    printf("The sum of its digits is %i.\n", c);
+
+    return 0;
+}
