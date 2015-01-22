1a106ad15dee1f3abe97bad10c1fe43e9ae14558
diegomg <dmartinezgarcia@outlook.com>
2015-01-22 23:04:34 +0100

    Add exercise 2.

---
+++ Lesson 4 - Program Looping/main.c
@@ -0,0 +1,30 @@
+/* NOTE: Select an exercise and use CTRL+MAYUS+C to comment it. Select an exercise and use CTRL+MAYUS+X to uncomment it. */
+
+/*********************************************************
+    Exercise 1
+		Type in and run the nine programs presented in this chapter. Compare the output produced by each program with the output presented after each program in the text.
+*********************************************************/
+
+// The outputs are the same.
+
+/*********************************************************
+    Exercise 2
+		Write a program to generate and display a table of n and n2, for integer values of n ranging from 1 to 10. Be certain to print appropriate column headings.
+*********************************************************/
+
+//#include <stdio.h>
+//
+//int main() {
+//    int n = 0, a = 0;
+//
+//    printf("Please, enter a positive integer number ranging from 1 to 10: ");
+//    scanf("%i", &n);
+//
+//    printf("\n");
+//
+//    for (a = 1; a <= n; a++) {
+//        printf("Number: %3i       Number squared: %3i\n", a, a * a);
+//    }
+//
+//    return 0;
+//}
