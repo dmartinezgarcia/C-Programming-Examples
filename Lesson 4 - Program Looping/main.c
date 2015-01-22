46d87b882a864b47685e3b957b37aabf08be5e75
diegomg <dmartinezgarcia@outlook.com>
2015-01-23 00:48:12 +0100

    Add exercise 10.

--- Lesson 4 - Program Looping/main.c
+++ Lesson 4 - Program Looping/main.c
@@ -192,3 +192,27 @@
 //
 //     return 0;
 //}
+
+/*********************************************************
+    Exercise 10
+        What would happen if you typed a negative number into Program 4.8? Try it and see.
+*********************************************************/
+
+// The minus symbol gets printed too, for example: as the modulus calculated with -123 is -3 -2 -1...
+
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//     int  number, right_digit;
+//     printf("Enter your number.\n");
+//     scanf("%i", &number);
+//     while (number != 0) {
+//          right_digit = number % 10;
+//          printf ("%i", right_digit);
+//          number = number / 10;
+//     }
+//     printf ("\n");
+//     return 0;
+//}
+
