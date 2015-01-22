c05d0c507ced631d707d4a94c81365c25a12683d
diegomg <dmartinezgarcia@outlook.com>
2015-01-23 00:19:10 +0100

    Add exercise 7. Added more infomration on field width specifiers.

--- Lesson 4 - Program Looping/main.c
+++ Lesson 4 - Program Looping/main.c
@@ -141,3 +141,26 @@
 //
 //    return 0;
 //}
+
+/*********************************************************
+    Exercise 7
+        A decimal point before the field width specification in a printf() statement has a special purpose.
+        Try to determine its purpose by typing in and running the following program. Experiment by typing in different values each time you are prompted.
+*********************************************************/
+
+#include <stdio.h>
+
+
+
+int main (void)
+{
+   int  dollars, cents, count;
+   for ( count = 1;  count <= 10;  ++count ) {
+        printf ("Enter dollars: ");
+        scanf ("%i", &dollars);
+        printf ("Enter cents: ");
+        scanf ("%i", &cents);
+        printf ("$%.3i.%.10i\n\n", dollars, cents);
+   }
+   return 0;
+}
