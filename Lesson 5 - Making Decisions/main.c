47903164301d6831fbb7f6671685e45f9391fbca
diegomg <dmartinezgarcia@outlook.com>
2015-01-24 00:58:18 +0100

    Added exercise 5.

--- Lesson 5 - Making Decisions/main.c
+++ Lesson 5 - Making Decisions/main.c
@@ -128,3 +128,42 @@
 //
 //    return 0;
 //}
+
+
+/*********************************************************
+    Exercise 5
+        You developed Program 4.9 to reverse the digits of an integer typed in from the terminal.
+        However, this program does not function well if you type in a negative number.
+        Find out what happens in such a case and then modify the program so that negative numbers are correctly handled.
+        For example, if the number −8645 is typed in, the output of the program should be 5468−.
+*********************************************************/
+
+//#include <stdio.h>
+//
+//int main ()
+//{
+//    int  number, right_digit;
+//    _Bool isNeg = 0;
+//
+//    printf ("Enter your number.\n");
+//    scanf ("%i", &number);
+//
+//    if (number < 0)
+//    {
+//        number = -number;
+//        isNeg = 1;
+//    }
+//
+//    do
+//    {
+//        right_digit = number % 10;
+//        printf ("%i", right_digit);
+//        number = number / 10;
+//    } while ( number != 0 );
+//
+//    if (isNeg)
+//        printf("-");
+//
+//    printf ("\n");
+//    return 0;
+//}
