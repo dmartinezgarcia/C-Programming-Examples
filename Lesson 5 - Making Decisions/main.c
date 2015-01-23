dfc8f1d77370aa1aaf203e9d644659e315035ce4
LI_dmartinez <dmartinez@linkinnova.com>
2015-01-23 14:08:49 +0100

    Add Exercise 3.

--- Lesson 5 - Making Decisions/main.c
+++ Lesson 5 - Making Decisions/main.c
@@ -13,19 +13,40 @@
         Write a program that asks the user to type in two integer values at the terminal. Test these two numbers to determine if the first is evenly divisible by the second, and then display an appropriate message at the terminal.
 *********************************************************/
 
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main ()
+//{
+//    int a = 0, b = 0;
+//
+//    printf("Please input two integer positive numbers: ");
+//    scanf("%i %i", &a, &b);
+//
+//    if (!(a % b))
+//        printf("\n%i is evenly divisable by %i", a, b);
+//    else
+//        printf("\n%i is NOT evenly divisable by %i", a, b);
+//
+//    return 0;
+//}
 
-int main ()
-{
-    int a = 0, b = 0;
+/*********************************************************
+    Exercise 3
+        Write a program that accepts two integer values typed in by the user. Display the result of dividing the first integer by the second, to three-decimal-place accuracy. Remember to have the program check for division by zero.
+*********************************************************/
 
-    printf("Please input two integer positive numbers: ");
-    scanf("%i %i", &a, &b);
-
-    if (!(a % b))
-        printf("\n%i is evenly divisable by %i", a, b);
-    else
-        printf("\n%i is NOT evenly divisable by %i", a, b);
-
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main ()
+//{
+//    int a = 0, b = 0;
+//
+//    printf("Please input two integer positive numbers: ");
+//    scanf("%i %i", &a, &b);
+//
+//    (b > 0) ?
+//            printf("Result of %i / %i is %.3f.\n", a, b, (float)a / b)
+//            : printf("Division by zero not allowed.\n");
+//
+//    return 0;
+//}
