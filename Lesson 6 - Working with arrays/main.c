7db37eeb399bb04bef2f4b024a98788f8f4ec570
diegomg <dmartinezgarcia@outlook.com>
2015-01-25 23:52:08 +0100

    Add exercises 1 and 2

---
+++ Lesson 6 - Working with arrays/main.c
@@ -0,0 +1,30 @@
+/* NOTE: Select an exercise and use CTRL+MAYUS+C to comment it. Select an exercise and use CTRL+MAYUS+X to uncomment it. */
+
+/*********************************************************
+    Exercise 1
+		Type in and run the eight programs presented in this chapter. Compare the output produced by each program with the output presented after each program in the text.
+*********************************************************/
+
+// The output is the same.
+
+/*********************************************************
+    Exercise 2
+		Modify Program 6.1 so that the elements of the array values are initially set to 0. Use a for loop to perform the initialization.
+*********************************************************/
+
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//     int  values[10];
+//     int  index;
+//
+//     for (index = 0; index < 10; ++index)
+//        values[index] = 0;
+//
+//     for ( index = 0;  index < 10;  ++index )
+//          printf ("values[%i] = %i\n", index, values[index]);
+//
+//     return 0;
+//}
+
