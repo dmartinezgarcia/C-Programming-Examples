e9f9823678ca1020b96d01b5a72bbf71fb3d3abb
diegomg <dmartinezgarcia@outlook.com>
2015-01-26 00:07:50 +0100

    Add Exercise 3.

--- Lesson 6 - Working with arrays/main.c
+++ Lesson 6 - Working with arrays/main.c
@@ -28,3 +28,45 @@
 //     return 0;
 //}
 
+/*********************************************************
+    Exercise 3
+		Program 6.2 permits only 20 responses to be entered. Modify that program so that any number of responses can be entered. So that the user does not have to count the number of responses in the list, set up the program so that the value 999 can be keyed in by the user to indicate that the last response has been entered. (Hint: You can use the break statement here if you want to exit your loop.)
+*********************************************************/
+
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//    int  ratingCounters[11], i, response, num;
+//
+//    for ( i = 1;  i <= 10;  ++i )
+//        ratingCounters[i] = 0;
+//
+//    printf ("Enter the number of responses: ");
+//    scanf("%i", &num);
+//    printf ("\nEnter your responses\n");
+//
+//    for ( i = 1;  i <= num;  ++i )
+//    {
+//        scanf ("%i", &response);
+//
+//        if (response == 999)
+//        {
+//            num = i - 1;
+//            printf("Last response entered.\n");
+//            break;
+//        }
+//        else if (response < 1 || response > 10)
+//            printf ("Bad response: %i\n", response);
+//        else
+//            ++ratingCounters[response];
+//    }
+//
+//    printf ("\n\nRating   Number of Responses\n");
+//    printf ("------ -------------------\n");
+//
+//    for ( i = 1;  i <= num;  ++i )
+//        printf ("%4i%14i\n", i, ratingCounters[i]);
+//
+//    return 0;
+//}
