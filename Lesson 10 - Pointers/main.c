cee447cb0dbf6c49a6ecac12fc2994d6e69baa85
dmartinezgarcia@outlook.com <diegomg>
2015-02-05 14:20:35 +0100

    Added exercise 1 and 2.

--- Lesson 10 - Pointers/main.c
+++ Lesson 10 - Pointers/main.c
@@ -5,10 +5,60 @@
 		Type in and run the 15 programs presented in this chapter. Compare the output produced by each program with the output presented after each program in the text.
 *********************************************************/
 
+// The output is the same.
+
 /*********************************************************
     Exercise 2
 		Write a function called insertEntry() to insert a new entry into a linked list. Have the procedure take as arguments a pointer to the list entry to be inserted (of type struct entry as defined in this chapter), and a pointer to an element in the list after which the new entry is to be inserted.
 *********************************************************/
+
+//#include <stdio.h>
+//
+//struct entry
+//{
+//    int value;
+//    struct entry* next;
+//};
+//
+//void insertEntry (struct entry* insEntry, struct entry* aftEntry)
+//{
+//    insEntry->next = aftEntry->next;
+//    aftEntry->next = insEntry;
+//}
+//
+//int main (void)
+//{
+//    struct entry n0, n1, n2;
+//    struct entry n1_2;
+//    struct entry* pointer = &n0;
+//
+//    n0 = (struct entry) {0, &n1};
+//    n1 = (struct entry) {1, &n2};
+//    n2 = (struct entry) {2, (struct entry*) 0};
+//    n1_2 = (struct entry) {3, (struct entry*) 0};
+//
+//
+//    printf("Before the insertion: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    insertEntry(&n1_2, &n2);
+//    pointer = &n0;
+//
+//    printf("After the insertion: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
 
 /*********************************************************
     Exercise 3
