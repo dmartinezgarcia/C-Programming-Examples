3e1a714171f64d3081792efd1e33923b542fa027
diegomg <dmartinezgarcia@outlook.com>
2015-02-06 03:00:52 +0100

    Up to exercise 6.

--- Lesson 10 - Pointers/main.c
+++ Lesson 10 - Pointers/main.c
@@ -65,20 +65,235 @@
 		The function developed in exercise 2 only inserts an element after an existing element in the list, thereby preventing you from inserting a new entry at the front of the list. How can you use this same function and yet overcome this problem? (Hint: Think about setting up a special structure to point to the beginning of the list.)
 *********************************************************/
 
+// The approach taken in this exercise was to set the first element of the linked list as a dummy element, the first important element on the list starts at index 1, so to speak.
+
+//#include <stdio.h>
+//
+//struct entry
+//{
+//    int value;
+//    struct entry* next;
+//};
+//
+//void insertEntry (struct entry* insEntry, struct entry* aftEntry)
+//{
+//    insEntry->next = aftEntry->next;
+//    aftEntry->next = insEntry;
+//}
+//
+//int main (void)
+//{
+//    struct entry begin, n0, n1, n2;
+//    struct entry n1_2;
+//    struct entry* pointer;
+//
+//    begin = (struct entry) {0, &n0};
+//    n0 = (struct entry) {1, &n1};
+//    n1 = (struct entry) {2, &n2};
+//    n2 = (struct entry) {3, (struct entry*) 0};
+//    n1_2 = (struct entry) {4, (struct entry*) 0};
+//
+//    pointer = begin.next;
+//
+//    printf("Before the insertion: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    insertEntry(&n1_2, &begin);
+//    pointer = begin.next;
+//
+//    printf("After the insertion: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
+
 /*********************************************************
     Exercise 4
 		Write a function called removeEntry() to remove an entry from a linked list. The sole argument to the procedure should be a pointer to the list. Have the function remove the entry after the one pointed to by the argument. (Why can’t you remove the entry pointed to by the argument?) You need to use the special structure you set up in exercise 3 to handle the special case of removing the first element from the list.
 *********************************************************/
+
+// The entry passed as argument can't be removed because you need to provide the precedent entry, which contains the pointer to the structure.
+
+//#include <stdio.h>
+//
+//struct entry
+//{
+//    int value;
+//    struct entry* next;
+//};
+//
+//void removeEntry (struct entry* remEntry)
+//{
+//    remEntry->next = remEntry->next->next;
+//}
+//
+//int main (void)
+//{
+//    struct entry begin, n0, n1, n2;
+//    struct entry* pointer;
+//
+//    begin = (struct entry) {0, &n0};
+//    n0 = (struct entry) {1, &n1};
+//    n1 = (struct entry) {2, &n2};
+//    n2 = (struct entry) {3, (struct entry*) 0};
+//
+//    pointer = begin.next;
+//
+//    printf("Before the removal: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    removeEntry(&n0);
+//    pointer = begin.next;
+//
+//    printf("After the removal: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
 
 /*********************************************************
     Exercise 5
 		A doubly linked list is a list in which each entry contains a pointer to the preceding entry in the list as well as a pointer to the next entry in the list. Define the appropriate structure definition for a doubly linked list entry and then write a small program that implements a small doubly linked list and prints out the elements of the list.
 *********************************************************/
 
+//#include <stdio.h>
+//
+//struct entry
+//{
+//    struct entry* previous;
+//    int value;
+//    struct entry* next;
+//};
+//
+//int main (void)
+//{
+//    struct entry n0, n1, n2;
+//    struct entry* pointer;
+//
+//    n0 = (struct entry) {(struct entry*) 0, 1, &n1};
+//    n1 = (struct entry) {&n0, 2, &n2};
+//    n2 = (struct entry) {&n1, 3, (struct entry*) 0};
+//
+//    pointer = &n0;
+//
+//    printf("Normal sequence: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    pointer = &n2;
+//
+//    printf("Reverse sequence: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->previous;
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
+
 /*********************************************************
     Exercise 6
 		Develop insertEntry() and removeEntry() functions for a doubly linked list that are similar in function to those developed in previous exercises for a singly linked list. Why can your removeEntry() function now take as its argument a direct pointer to the entry to be removed from the list?
 *********************************************************/
+
+// Now you can remove the entry passed as argument because the pointer to the previous element is passed along with the structure.
+
+//#include <stdio.h>
+//
+//struct entry
+//{
+//    struct entry* previous;
+//    int value;
+//    struct entry* next;
+//};
+//
+//void insertEntry (struct entry* insEntry, struct entry* aftEntry)
+//{
+//    insEntry->previous = aftEntry;
+//    insEntry->next = aftEntry->next;
+//    aftEntry->next->previous = insEntry;
+//    aftEntry->next = insEntry;
+//}
+//
+//void removeEntry (struct entry* remEntry)
+//{
+//    remEntry->next->previous = remEntry->previous;
+//    remEntry->previous->next = remEntry->next;
+//}
+//
+//int main (void)
+//{
+//    struct entry begin, n0, n1, n2;
+//    struct entry n1_2;
+//    struct entry* pointer;
+//
+//    begin = (struct entry) {(struct entry*) 0, 0, &n0};
+//    n0 = (struct entry) {&begin, 1, &n1};
+//    n1 = (struct entry) {&n0, 2, &n2};
+//    n2 = (struct entry) {&n1, 3, (struct entry*) 0};
+//
+//    n1_2 = (struct entry) {(struct entry*) 0, 4, (struct entry*) 0};
+//
+//    pointer = begin.next;
+//
+//    printf("Normal: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    insertEntry(&n1_2, &begin);
+//    pointer = begin.next;
+//
+//    printf("After add: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    removeEntry(&n1_2);
+//    pointer = begin.next;
+//
+//    printf("Removed: \n");
+//    while (pointer)
+//    {
+//        printf("Value in linked list entry: %i.\n", pointer->value);
+//        pointer = pointer->next;
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
 
 /*********************************************************
     Exercise 7
