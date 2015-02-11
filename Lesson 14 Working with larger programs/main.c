d917e54abd581942cb429d6039b2bfda99efa341
dmartinezgarcia@outlook.com <diegomg>
2015-02-11 11:24:46 +0100

    Finished lesson 14.

---
+++ Lesson 14 Working with larger programs/main.c
@@ -0,0 +1,8 @@
+/* NOTE: Select an exercise and use CTRL+MAYUS+C to comment it. Select an exercise and use CTRL+MAYUS+X to uncomment it. */
+
+/*********************************************************
+    Exercise X
+		Description
+*********************************************************/
+
+// There are no exercises for this lesson.
